70abad31baf5ff62ec70cceea8c5248eea73b100
Mehdi Louzar <mehdilouzar1@gmail.com>
2022-11-24 21:26:42 +0100

    add&post methods for authentication

--- hotelio/Project/hotelio/Controllers/AuthentificationController.cs
+++ hotelio/Project/hotelio/Controllers/AuthentificationController.cs
@@ -9,13 +9,26 @@ namespace hotelio.Controllers
     public class AuthentificationController : Controller
     {
         // GET: Authentification
-
+        [HttpGet]
         public ActionResult Login()
         {
             return View();
         }
 
+        [HttpGet]
         public ActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login_Post()
+        {
+            return Redirect("/Home/Index");
+        }
+
+        [HttpPost]
+        public ActionResult Register_Post()
         {
             return View();
         }
